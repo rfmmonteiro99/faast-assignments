56c5229552a571bb3df5b0e827d51e4573f0c50d
Rui Monteiro <rui.m.monteiro@nos.pt>
2023-04-05 17:13:40 +0100

    Fix and improve the pytest for cleaning.py

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,15 +1,16 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import main
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(
-        file_path='./life_expectancy/data/eu_life_expectancy_raw.tsv', 
-        region='PT'
+    main(
+        input_filename='./life_expectancy/data/eu_life_expectancy_raw.tsv', 
+        region='PT',
+        output_filename='./life_expectancy/data/pt_life_expectancy.csv'
     )
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
