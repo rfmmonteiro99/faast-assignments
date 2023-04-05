f96dc0b2086a90c744cf26aca9276fc04daab105
Rui Monteiro <rui.m.monteiro@nos.pt>
2023-04-05 16:41:13 +0100

    Improve readability and structure on cleaning.py

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -6,15 +6,13 @@ import pandas as pd
 
 
 def load_data(
-    file_path: str
-) -> pd.DataFrame:
+        file_path: str) -> pd.DataFrame:
     """
-    Loads file as a pandas DataFrame
+    Loads file from its folder as a pandas DataFrame
     :param file_name: Name of the file to be loaded
 
     :return: Pandas DataFrame with the loaded data
     """
-    # Load the eu_life_expectancy_raw.tsv data from the data folder
     data = pd.read_csv(
         file_path,
         sep='\t'
@@ -23,9 +21,8 @@ def load_data(
     return data
 
 def clean_data(
-    eu_life_expectancy: pd.DataFrame,
-    region: str = 'PT'
-) -> pd.DataFrame:
+        eu_life_expectancy: pd.DataFrame,
+        region: str = 'PT') -> pd.DataFrame:
     """
     Clean the previously imported DataFrame
     :param eu_life_expectancy: Pandas DataFrame with life expectancy data
@@ -55,22 +52,24 @@ def clean_data(
     )
 
     # Change column name
-    eu_life_expectancy_pivot = eu_life_expectancy_pivot\
-        .rename(columns={eu_life_expectancy_pivot.columns[3]: 'region'})
+    eu_life_expectancy_pivot = (
+        eu_life_expectancy_pivot.rename(columns={eu_life_expectancy_pivot.columns[3]: 'region'})
+    )
 
     # Change year's data type
     eu_life_expectancy_pivot = eu_life_expectancy_pivot.astype({'year': int})
 
     # Get only the float numbers in the 'value' column and set the correspondent data type
-    eu_life_expectancy_pivot['value'] = eu_life_expectancy_pivot['value']\
-        .str\
-        .extract(r'([0-9]+\.?[0-9])')\
-        .astype('float')
+    eu_life_expectancy_pivot['value'] = (
+        eu_life_expectancy_pivot['value'].str.extract(r'([0-9]+\.?[0-9])').astype('float')
+    )
 
     # Drop missing values
-    eu_life_expectancy_final = eu_life_expectancy_pivot[~eu_life_expectancy_pivot['value'].isna()]
+    eu_life_expectancy_final = eu_life_expectancy_pivot[
+        ~eu_life_expectancy_pivot['value'].isna()
+    ]
 
-    # Filter the data where region equal to PT (Portugal)
+    # Filter the data for `region`
     eu_life_expectancy_final = eu_life_expectancy_final[
         eu_life_expectancy_final['region'] == region
     ]
@@ -78,37 +77,51 @@ def clean_data(
     return eu_life_expectancy_final
 
 def save_data(
-    data_to_save: pd.DataFrame,
-    file_name: str
-):
+        data_to_save: pd.DataFrame,
+        file_name: str):
     """
     Save the clean DataFrame to a specified folder
     :param data_to_save: Pandas DataFrame with the data to be saved
     :param file_name: Name for the created file
     """
-    # Save the resulting dataframe to the data folder
     data_to_save.to_csv(
         file_name,
         index=False
     )
 
-if __name__ == "__main__": # pragma: no cover
-
-    # Parse arguments passed through the command-line
-    parser = argparse.ArgumentParser()
-    parser.add_argument('region')
-    args = parser.parse_args()
-
+def main(
+        input_filename: str,
+        region: str,
+        output_filename: str):
+    """
+    Steps: Load the data, clean it, and export it
+    :param input_filename: 
+    :param region: 
+    :param output_filename: 
+    """
     loaded_data = load_data(
-        file_path='life_expectancy/data/eu_life_expectancy_raw.tsv'
+        file_path=input_filename
     )
 
     cleaned_data = clean_data(
         eu_life_expectancy=loaded_data,
-        region=args.region
+        region=region
     )
 
     save_data(
         cleaned_data,
+        output_filename
+    )
+
+
+if __name__ == "__main__": # pragma: no cover
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('region')
+    args = parser.parse_args()
+
+    main(
+        'life_expectancy/data/eu_life_expectancy_raw.tsv',
+        args.region,
         'life_expectancy/data/pt_life_expectancy.csv'
     )
