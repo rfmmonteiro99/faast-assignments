2962dc97c88ce1bde94f97545a542ed59941d51f
Rui Monteiro <rui.m.monteiro@nos.pt>
2023-05-08 17:48:31 +0100

    Test main

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,28 +1,17 @@
 """Tests for the cleaning module"""
-import pytest
 from unittest.mock import patch, Mock
 import pandas as pd
 
-from life_expectancy.tests.fixtures.sample_data import raw_data, expected_result
-from life_expectancy.cleaning import load_data, clean_data, save_data
+from life_expectancy.cleaning import load_data, clean_data, save_data, main
 from . import OUTPUT_DIR
 
-
-@pytest.fixture
-def fixture_raw_data():
-    """Load raw data"""
-    return pd.DataFrame(raw_data())
-
-@pytest.fixture
-def fixture_expect():
-    """Load expected result"""
-    return pd.DataFrame(expected_result())
 
 @patch('life_expectancy.cleaning.pd.read_csv')
 def test_load_data(read_table_mock: Mock, fixture_raw_data):
     """Run the `load_data` function"""
     read_table_mock.return_value = fixture_raw_data
-    results = load_data(OUTPUT_DIR, 'eu_life_expectancy_raw.tsv')
+    file_name = 'eu_life_expectancy_raw.tsv'
+    results = load_data(OUTPUT_DIR / file_name)
     read_table_mock.assert_called_once()
 
     pd.testing.assert_frame_equal(results, fixture_raw_data)
@@ -40,14 +29,25 @@ def test_clean_data(fixture_raw_data, fixture_expect):
 
 def test_save_data(fixture_expect, capfd):
     """Run the `save_data` function"""
+    
     with patch.object(fixture_expect, 'to_csv') as to_csv_mock:
         to_csv_mock.side_effect = print('Data saved', end='')
+        file_name = 'pt_life_expectancy.csv'
         save_data(
             fixture_expect,
-            OUTPUT_DIR,
-            'pt_life_expectancy.csv'
+            OUTPUT_DIR / file_name
         )
         to_csv_mock.assert_called_once()
 
         out, _ = capfd.readouterr()
         assert out == 'Data saved'
+
+@patch("life_expectancy.cleaning.load_data")
+def test_main(load_mock: Mock, fixture_raw_data, fixture_expect):
+    """Run the `main` function and compare the output to the expected output"""
+    load_mock.return_value = fixture_raw_data
+    pt_life_expectancy_data = main('PT').reset_index(drop=True)
+
+    pd.testing.assert_frame_equal(
+        pt_life_expectancy_data, fixture_expect
+    )
